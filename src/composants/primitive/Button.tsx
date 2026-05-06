f64122e06260f929b4063380da3992226ddfe5fc
Ambroise Donnet <ambroise.donnet.etu@univ-lille.fr>
2026-05-06 09:59:54 +0200

    Ajout du composant Button

---
+++ src/composants/primitive/Button.tsx
@@ -0,0 +1,18 @@
+type ButtonProps = {
+    variant?: 'primary' | 'secondary' | 'ghost' | 'danger';
+    size?: 'sm' | 'md' | 'lg';
+    isDisabled?: boolean;
+    onClick?: () => void;
+    children?: React.ReactNode;
+}
+export default function Button(props: ButtonProps) {
+
+    const variantClass = props.variant ? `btn-${props.variant}` : 'btn-primary';
+    const sizeClass = props.size ? `btn-${props.size}` : 'btn-md';
+
+    return (
+        <button className={`btn ${variantClass} ${sizeClass}`} disabled={props.isDisabled} onClick={props.onClick}>
+            {props.children || "Button"}
+        </button>
+    )
+}
